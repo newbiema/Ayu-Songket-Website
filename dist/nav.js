97c0460cf6179529b036387f1df9f912d77d5218
Evan Jamaq <113580904+newbiema@users.noreply.github.com>
2024-05-23 09:53:08 +0700

    nav.js
    Update setelah di klik nav kembali ke mode hamburger menu

--- dist/nav.js
+++ dist/nav.js
@@ -1,4 +1,16 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var nav = document.getElementById('navbar-default');
-    nav.classList.toggle('hidden');
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navbar = document.getElementById('navbar-default');
+    const navItems = document.querySelectorAll('.nav-item');
+
+    menuToggle.addEventListener('click', function () {
+        navbar.classList.toggle('hidden');
+    });
+
+    navItems.forEach(item => {
+        item.addEventListener('click', function () {
+            // Close the navbar
+            navbar.classList.add('hidden');
+        });
+    });
 });
